dc92366e7e17a4b4b5da8f10eadae1482f191484
guimaraes05 <bicas.guimaraes5@gmail.com>
2018-09-28 17:22:19 -0300

    ajustando mongodb para nuvem

--- src/main/java/com/pontinhos/config/MongoConfig.java
+++ src/main/java/com/pontinhos/config/MongoConfig.java
@@ -7,22 +7,21 @@ import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoOperations;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 @Configuration
 public class MongoConfig extends AbstractMongoConfiguration {
 
 	@Override
 	public MongoClient mongoClient() {
-		return new MongoClient("mongodb://heroku_zftqfprt:he3ruo2be98ocjec2ohc9s3ht0@ds031651.mlab.com:31651/heroku_zftqfprt");
+		return new MongoClient(new MongoClientURI("mongodb://teste:guimaraes05@ds117773.mlab.com:17773/heroku_d6f9mb39"));
 	}
 
 	@Override
 	protected String getDatabaseName() {
-		return "heroku_zftqfprt";
+		return "heroku_d6f9mb39";
 	}
 
-	
-	
 	public ApplicationContext applicationContext() {
 		return new AnnotationConfigApplicationContext(MongoConfig.class);
 	}
